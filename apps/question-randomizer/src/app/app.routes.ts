82ac8600d7b82e86700982f60dca7083527c3cb7
bartosz-laszczuk <bartosz.laszczuk@outlook.com>
2025-03-17 19:56:44 +0100

    clean up

--- apps/question-randomizer/src/app/app.routes.ts
+++ apps/question-randomizer/src/app/app.routes.ts
@@ -1,3 +1,59 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('@regsys/sppr/dashboard/shell').then((r) => r.routes),
+  },
+  //   {
+  //     path: '',
+  //     children: [
+  //       {
+  //         path: '',
+  //         pathMatch: 'full',
+  //         redirectTo: 'randomization',
+  //       },
+  //       {
+  //         path: 'questions',
+  //         loadChildren: () =>
+  //           import('../questions/questions.module').then(
+  //             (m) => m.QuestionsModule
+  //           ),
+  //         canActivate: [AuthCanActivate],
+  //       },
+  //       {
+  //         path: 'settings',
+  //         loadComponent: () =>
+  //           import('../settings/settings.component').then(
+  //             (m) => m.SettingsComponent
+  //           ),
+  //         canActivate: [AuthCanActivate],
+  //       },
+  //       // {
+  //       //   path: 'demo',
+  //       //   loadChildren: () =>
+  //       //     import('../demo/demo.module').then((m) => m.DemoModule),
+  //       // },
+  //       // {
+  //       //   path: 'auth',
+  //       //   loadChildren: () =>
+  //       //     import('../auth/auth.module').then((m) => m.AuthModule),
+  //       // },
+  //       {
+  //         path: 'randomization',
+  //         loadChildren: () =>
+  //           import('../randomization/randomization.module').then(
+  //             (m) => m.RandomizationModule
+  //           ),
+  //         canActivate: [AuthCanActivate],
+  //       },
+  //       {
+  //         path: 'static',
+  //         loadChildren: () =>
+  //           import('../static/static.module').then((m) => m.StaticModule),
+  //       },
+  //     ],
+  //   },
+  //   { path: '**', pathMatch: 'full', redirectTo: '/static/404' },
+];
